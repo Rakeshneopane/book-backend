6b3ca170043feb815ae7289aec9723b9a8b8388f
rakeshneopane <rakeshneopane@gmail.com>
2025-10-07 18:35:54 +0530

    fix: Added cors config

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
 app.use(cors());
 
 
